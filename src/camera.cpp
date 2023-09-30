0d7287ab8a0f8539949405ad3c2c960ca1f0bd6c
jastew15 <jastew15@gmail.com>
2023-09-29 19:28:31 -0500

    fix register types, start camera

---
+++ src/camera.cpp
@@ -0,0 +1,15 @@
+#include "camera.h"
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/random_number_generator.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+#include <cstdlib>
+
+using namespace godot;
+
+void Camera::_bind_methods() {}
+
+Camera::Camera() {}
+
+Camera::~Camera() {}
+
+void Camera::_process(double delta) {}
---
+++ src/camera.h
@@ -0,0 +1,24 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <godot_cpp/classes/camera3d.hpp>
+
+namespace godot {
+
+class Camera : public Camera3D {
+    GDCLASS(Camera, Camera3D)
+
+private:
+
+protected:
+    static void _bind_methods();
+
+public:
+    Camera();
+    ~Camera();
+    void _process(double delta) override;
+};
+
+}
+
+#endif
