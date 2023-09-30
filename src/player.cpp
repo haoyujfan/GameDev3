f7eceaead33a0795b8f64e2df0dfedd2ec397e57
mtnp <silasp0801@gmail.com>
2023-09-30 04:55:51 +0000

    Update file player.cpp

--- src/player.cpp
+++ src/player.cpp
@@ -27,8 +27,17 @@ void Player::_input(const Ref<InputEvent> &event) {
     if (key_event) {
         UtilityFunctions::print(key_event);
         if (key_event->as_text_key_label() == "W") {
-            position += Vector3(0.0, 0.0, 1.0);
-            set_position(position);
+            position += Vector3(0.0, 0.0, -1.0);
         }
+        if (key_event->as_text_key_label() == "S") {
+            position += Vector3(0.0, 0.0, 1.0);
+        }
+        if (key_event->as_text_key_label() == "A") {
+            position += Vector3(-1.0, 0.0, 0.0);
+        }
+        if (key_event->as_text_key_label() == "D") {
+            position += Vector3(1.0, 0.0, 0.0);
+        }
+        set_position(position);
     }
 }
