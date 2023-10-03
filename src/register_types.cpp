66c45c54079b7a3ce25d694ce59d4ab3165c20cf
jastew15 <jastew15@gmail.com>
2023-10-03 09:45:12 -0500

    condense register types (should help with compilation time)

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -16,61 +16,13 @@ void initialize_player_module(ModuleInitializationLevel p_level) {
         return;
     }
     ClassDB::register_class<Player>();
-}
-
-void uninitialize_player_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
-}
-
-void initialize_cactus_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
     ClassDB::register_class<Cactus>();
-}
-
-void uninitialize_cactus_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
-}
-
-void initialize_food_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
     ClassDB::register_class<Food>();
-}
-
-void uninitialize_food_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
-}
-
-void initialize_ground_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
     ClassDB::register_class<Ground>();
-}
-
-void uninitialize_ground_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
-}
-
-void initialize_camera_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
     ClassDB::register_class<Camera>();
 }
 
-void uninitialize_camera_module(ModuleInitializationLevel p_level) {
+void uninitialize_player_module(ModuleInitializationLevel p_level) {
     if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
         return;
     }
@@ -87,45 +39,4 @@ GDExtensionBool GDE_EXPORT player_library_init(GDExtensionInterfaceGetProcAddres
 
     return init_obj.init();
 }
-
-GDExtensionBool GDE_EXPORT cactus_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-    
-    init_obj.register_initializer(initialize_cactus_module);
-    init_obj.register_terminator(uninitialize_cactus_module);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
-    return init_obj.init();
-}
-
-GDExtensionBool GDE_EXPORT food_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-
-    init_obj.register_initializer(initialize_food_module);
-    init_obj.register_terminator(uninitialize_food_module);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
-    return init_obj.init();
-}
-
-GDExtensionBool GDE_EXPORT ground_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-    
-    init_obj.register_initializer(initialize_ground_module);
-    init_obj.register_terminator(uninitialize_ground_module);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
-    return init_obj.init();
-}
-
-GDExtensionBool GDE_EXPORT camera_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
-    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
-
-    init_obj.register_initializer(initialize_camera_module);
-    init_obj.register_terminator(uninitialize_camera_module);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
-
-    return init_obj.init();
-}
-
 }
--- src/register_types.h
+++ src/register_types.h
@@ -1,34 +1,14 @@
 #ifndef PLAYER_REGISTER_TYPES_H
 #define PLAYER_REGISTER_TYPES_H
 
-void initialize_player_module();
-void uninitialize_player_module();
-
-#endif
-
 #ifndef CACTUS_REGISTER_TYPES_H
 #define CACTUS_REGISTER_TYPES_H
-
-void initialize_cactus_module();
-void uninitialize_cactus_module();
-
-#endif
 
 #ifndef CAMERA_REGISTER_TYPES_H
 #define CAMERA_REGISTER_TYPES_H
 
-void initialize_camera_module();
-void uninitialize_camera_module();
-
-#endif
-
 #ifndef FOOD_REGISTER_TYPES_H
 #define FOOD_REGISTER_TYPES_H
-
-void initialize_food_module();
-void uninitialize_food_module();
-
-#endif
 
 #ifndef GROUND_REGISTER_TYPES_H
 #define GROUND_REGISTER_TYPES_H
@@ -36,4 +16,8 @@ void uninitialize_food_module();
 void initialize_player_module();
 void uninitialize_player_module();
 
+#endif
+#endif
+#endif
+#endif
 #endif
